8cc5eca2bd9fe4d1abb2898cc4cbe9daac743f9a
Camilafabiani87 <54865542+Camilafabiani87@users.noreply.github.com>
2022-12-23 14:44:04 -0300

    Initial commit

---
+++ src/main/java/ChallengeFinal/service/implement/PDFServiceImplement.java
@@ -0,0 +1,87 @@
+package ChallengeFinal.service.implement;
+
+import ChallengeFinal.models.*;
+import ChallengeFinal.repository.BuyerRepository;
+import ChallengeFinal.service.PdfService;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class PDFServiceImplement implements PdfService {
+    @Autowired
+    BuyerRepository buyerRepository;
+
+    @Override
+    public void export(Buyer buyer, TicketPurchase ticketPurchase, HttpServletResponse response) throws IOException, DocumentException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document,response.getOutputStream());
+        document.open();
+
+        Font fontTitle = FontFactory.getFont(FontFactory.TIMES_BOLD);
+        fontTitle.setSize(18);
+
+        Paragraph title = new Paragraph("Purchases", fontTitle);
+        title.setAlignment(Paragraph.ALIGN_CENTER);
+        Font fontParagraph = FontFactory.getFont(FontFactory.TIMES_ROMAN);
+        fontParagraph.setSize(12);
+        Paragraph subtitle = new Paragraph("Client: "+ buyer.getEmail(), fontParagraph);
+        subtitle.setAlignment(Paragraph.ALIGN_CENTER);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+        Paragraph purchaseDate = new Paragraph("Date: " + dateTimeFormatter.format(ticketPurchase.getPurchaseDate()), fontParagraph);
+        Paragraph finalPrice = new Paragraph("Total: $ " +ticketPurchase.getPriceFinal(), fontParagraph);
+        finalPrice.setAlignment(Paragraph.ALIGN_RIGHT);
+        Paragraph space = new Paragraph("\n");
+
+//        com.lowagie.text.Image image = null;
+//        image = Image.getInstance("/src/main/resources/static/web/assets/img/logo.png");
+//        image.scaleAbsolute(100,100);
+//        image.setAbsolutePosition(100, 100);
+
+        PdfPTable table = new PdfPTable(4);
+        Stream.of("Brand","Product","Type","Price").forEach(table::addCell);
+        List<Ticket_Accessory> ticket_accessories = ticketPurchase.getTicketAccesories();
+        ticket_accessories.forEach(ticket_accessory -> {
+            table.addCell(ticket_accessory.getAccessory().getBrand());
+            table.addCell(ticket_accessory.getAccessory().getModel());
+            table.addCell("Accessory");
+            table.addCell("$ " + ticket_accessory.getAccessory().getPrice().toString());
+        });
+
+        List<Ticket_Phone> ticket_phones = ticketPurchase.getTicketPhones();
+        ticket_phones.forEach(ticket_phone -> {
+            table.addCell(ticket_phone.getPhone().getBrand());
+            table.addCell(ticket_phone.getPhone().getModel());
+            table.addCell("Phone");
+            table.addCell("$ " + ticket_phone.getPhone().getPrice().toString());
+        });
+
+        List<Ticket_Console> ticket_consoles = ticketPurchase.getTicketConsoles();
+        ticket_consoles.forEach(ticket_console -> {
+            table.addCell(ticket_console.getConsole().getBrand());
+            table.addCell(ticket_console.getConsole().getModel());
+            table.addCell("Accessory");
+            table.addCell("$ " + ticket_console.getConsole().getPrice());
+        });
+
+//        document.add(image);
+        document.add(title);
+        document.add(space);
+        document.add(subtitle);
+        document.add(purchaseDate);
+        document.add(space);
+        document.add(space);
+        document.add(table);
+        document.add(space);
+        document.add(finalPrice);
+        document.close();
+    }
+}
